2a949e8ab0aed66e355c8c26fabd837b7584191a
Yashasvi Tomar <yt.yashasvii@gmail.com>
2026-07-13 18:54:45 +0530

    Time: 0 ms (100%), Space: 31.4 MB (46.38%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int low= 0;
+        int high = n-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(nums[mid]==target) return mid;
+            if(target>nums[mid]){
+                low = low + 1;
+            }
+            else{
+                high = high - 1;
+            }
+        }
+        return -1;
+    }
+};
