e1024a033726e4a2976cf402c124d369f9996ea9
Yashasvi Tomar <yt.yashasvii@gmail.com>
2026-09-17 12:37:44 +0530

    Time: 0 ms (100%), Space: 18.1 MB (33.84%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int,int>mp;
+        vector<pair<int,int>>ans;
+        
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]++;
+        }
+        for(auto x:mp){
+            ans.push_back({x.second, x.first});
+        }
+        sort(ans.begin(), ans.end(), greater<pair<int,int>>());
+        vector<int> result;
+        for(int i = 0; i < k; i++) {
+            result.push_back(ans[i].second);
+        }
+
+        return result;
+    }
+};
