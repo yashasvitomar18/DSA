58b4acf05106aafa826402162f4bf449eddcaab6
Yashasvi Tomar <yt.yashasvii@gmail.com>
2026-09-19 11:00:04 +0530

    Time: 6 ms (21.8%), Space: 9.8 MB (48.32%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,20 +1,11 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.length()!=t.length()) return false;
-
-        int freq[26] = {0};
-
-        for(int i =0;i<s.length();i++){
-        freq[s[i]-'a']++;
-        freq[t[i]-'a']--;
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        if(s==t){
+            return true;
         }
-        
-        for(int i = 0;i<26;i++){
-            if(freq[i]!=0){
-                return false;
-            }
-        }
-        return true;
+        return false;
     }
 };
