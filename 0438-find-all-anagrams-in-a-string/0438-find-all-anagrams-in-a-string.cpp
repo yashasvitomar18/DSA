7f80de3aff537032df0f611e4923231f155f92b3
Yashasvi Tomar <yt.yashasvii@gmail.com>
2026-09-20 21:35:33 +0530

    Time: 18 ms (27.06%), Space: 14.9 MB (19.12%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -1,34 +1,41 @@
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-
         vector<int> ans;
 
-        int l = 0;
-        int m = p.length();
+        if (p.length() > s.length())
+            return ans;
 
-        unordered_map<char, int> mp1;
-        unordered_map<char, int> mp2;
+        unordered_map<char, int> pmap;
+        unordered_map<char, int> smap;
 
-        for (char c : p) {
-            mp1[c]++;
+        // Frequency of p
+        for (char ch : p) {
+            pmap[ch]++;
         }
 
-        for (int r = 0; r < s.length(); r++) {
-            mp2[s[r]]++;
+        int left = 0;
 
-            if (r - l + 1 == m) {
-                if (mp1 == mp2) {
-                    ans.push_back(l);
+        for (int right = 0; right < s.length(); right++) {
+
+            // Add current character
+            smap[s[right]]++;
+
+            // Window size p se bada ho gaya
+            if (right - left + 1 > p.length()) {
+                smap[s[left]]--;
+
+                if (smap[s[left]] == 0)
+                    smap.erase(s[left]);
+
+                left++;
+            }
+
+            // Same frequency => anagram
+            if (right - left + 1 == p.length()) {
+                if (smap == pmap) {
+                    ans.push_back(left);
                 }
-
-                mp2[s[l]]--;
-
-                if (mp2[s[l]] == 0) {
-                    mp2.erase(s[l]);
-                }
-
-                l++;
             }
         }
 
