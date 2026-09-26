f8a8d1b5b63cdf3de73fb58b942c3978bb6360d6
Yashasvi Tomar <yt.yashasvii@gmail.com>
2026-09-26 22:54:58 +0530

    Time: 0 ms (100%), Space: 8.4 MB (99.65%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        int i=0;
-        int j=0;
-        while(i<s.size() && j<t.size()){
-            if(s[i]==t[j]){
-                i++;
-            }
-         j++;
+        int j = 0;
+        for(int i = 0;i<t.size();i++){
+            if(s[j]==t[i])
+            j++;
         }
-        return i==s.size();
+        return j==s.size();
     }
 };
